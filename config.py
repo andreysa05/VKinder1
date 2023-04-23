fd2bb0ba2f5c5d733fb810620d2987a406f31ebd
andreysa05 <130864729+andreysa05@users.noreply.github.com>
2023-04-23 13:07:08 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+
+user_token= 'https://oauth.vk.com/blank.html#access_token=vk1.a.CeY740jzaHhCIKD2x-k8760BvmnRdKkpjQsoy73e11h2VUMBh_msKHv8S7Q_sZGD2QoZu4sDFIgDQJ3XNUQfVek1ebXtkIeB1aFmx-KNl0VmWJSkWgIwxmhNxyjxRQl9_3ExXMpNOtQFlQgN8r6g-wHFPyfRguqFJ2fUPe6KeR95uN7ZrWyJjzsswuLNFu6pa-y-7oaogKDA0wxKdqBYHQ&expires_in=86400&user_id=400286610'
+group_token ='vk1.a.QzBodhN0Vc-A3rfMrej9ycU8EwqU5o1tJvBQpsWSwNONNUEO_hcsgW50ZMieKtgyXl9pTWhx4BKMmXPet7sY93ku3hsWKw7rlcR2c547YonIhV_odhmC7_P2gNt0SPE2NALNJ8giXxZ3gmOsGKxiZuTff9zDvh_eq4r0DKZFn4Hmhfh2R6fEokfJlTDPvK9ZFFgeuJzZBor80PNW4PcPhg'
+
+
+
+host = '127.0.0.1'
+user = 'postgres'
+password = ''
+db_title = ''
