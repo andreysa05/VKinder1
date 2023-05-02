c6352fac8825bdd33e9f6d93d51f40bd77818b73
andreysa05 <130864729+andreysa05@users.noreply.github.com>
2023-05-02 19:06:22 +0300

    Update db.py

--- db.py
+++ db.py
@@ -2,45 +2,37 @@ import psycopg2 as pg
 from config import host, user, password, db_title
 
 
-def create_table_seen_person():
-    with pg.connect(host=host, user=user, password=password, database=db_title) as conn:
-        conn.autocommit = True
-        with conn.cursor() as cursor:
-            cursor.execute(
-                """CREATE TABLE IF NOT EXISTS seen_person(
-                id serial,
-                id_vk varchar(50) PRIMARY KEY);"""
-            )
+class DatabaseManager:
+    def __init__(self):
+        self.connection = pg.connect(host=host, user=user, password=password, database=db_title)
+        self.connection.autocommit = True
+        self.cursor = self.connection.cursor()
+
+    def create_table_seen_person(self):
+        self.cursor.execute(
+            """CREATE TABLE IF NOT EXISTS seen_person(
+            id serial,
+            id_vk varchar(50) PRIMARY KEY);"""
+        )
+
+    def insert_data_seen_person(self, id_vk):
+        self.cursor.execute(
+            """INSERT INTO seen_person (id_vk) VALUES (%s)""",
+            (id_vk,),
+        )
+
+    def check(self):
+        self.cursor.execute("""SELECT sp.id_vk FROM seen_person AS sp;""")
+        return self.cursor.fetchall()
+
+    def delete_table_seen_person(self):
+        self.cursor.execute("""DROP TABLE IF EXISTS seen_person CASCADE;""")
+
+    def __del__(self):
+        self.cursor.close()
+        self.connection.close()
 
 
-def insert_data_seen_person(id_vk):
-    with pg.connect(host=host, user=user, password=password, database=db_title) as conn:
-        conn.autocommit = True
-        with conn.cursor() as cursor:
-            cursor.execute(
-                f"""INSERT INTO seen_person (id_vk) 
-               VALUES (%s)""",
-                (id_vk,),
-            )
-
-
-def check():
-    with pg.connect(host=host, user=user, password=password, database=db_title) as conn:
-        conn.autocommit = True
-        with conn.cursor() as cursor:
-            cursor.execute(
-                f"""SELECT sp.id_vk
-                FROM seen_person AS sp;"""
-            )
-            return cursor.fetchall()
-
-
-def delete_table_seen_person():
-    with pg.connect(host=host, user=user, password=password, database=db_title) as conn:
-        conn.autocommit = True
-        with conn.cursor() as cursor:
-            cursor.execute("""DROP TABLE  IF EXISTS seen_person CASCADE;""")
-
-
-create_table_seen_person()
+database_manager = DatabaseManager()
+database_manager.create_table_seen_person()
 print("Database was created!")
